8c72bcdf997475677947f392479e66ea25f4ebcb
Ryan Wilke <ryanwilke@gmail.com>
2021-05-16 23:47:18 +0200

    Delete logo.spec.js

--- components/logo/logo.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@cypress/vue'
-import Logo from './logo.vue'
-
-describe('Logo', () => {
-  it('contains an svg', () => {
-    mount(<Logo />)
-    cy.get('svg').should('exist')
-  })
-})
