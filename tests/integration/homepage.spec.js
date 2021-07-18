17caeaa25635ebc5feddc9241950380660664c43
Ryan Wilke <ryanwilke@gmail.com>
2021-07-18 14:48:05 +0200

    Update homepage.spec.js

--- tests/integration/homepage.spec.js
+++ tests/integration/homepage.spec.js
@@ -1,7 +1,13 @@
 describe('Homepage', () => {
-  it('Visit Homepage', () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
+
+  it('Content is accurate and able to be interacted with', () => {
+    cy.get('svg.NuxtLogo')
     cy.contains('h1', 'Nuxt.js Template')
+    cy.contains('a.button--green', 'Documentation').click()
+    cy.contains('a.button--grey', 'GitHub').click()
     cy.screenshot('homepage')
   })
 })
