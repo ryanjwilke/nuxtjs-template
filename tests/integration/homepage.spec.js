c02abbf50e31794c97619442b524d271cea9fc7d
Ryan Wilke <ryanwilke@gmail.com>
2021-07-18 18:38:04 -0400

    Fixing some stuff

--- tests/integration/homepage.spec.js
+++ tests/integration/homepage.spec.js
@@ -6,8 +6,8 @@ describe('Homepage', () => {
   it('Content is accurate and able to be interacted with', () => {
     cy.get('svg.NuxtLogo')
     cy.contains('h1', 'Nuxt.js Template')
-    cy.contains('a.button--green', 'Documentation').click()
-    cy.contains('a.button--grey', 'GitHub').click()
+    cy.contains('a.button--green', 'Documentation').should('be.visible')
+    cy.contains('a.button--grey', 'GitHub').should('be.visible')
     cy.screenshot('homepage')
   })
 })
